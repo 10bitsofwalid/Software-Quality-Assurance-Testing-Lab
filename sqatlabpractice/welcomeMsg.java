a857227da6fe43c236201d16e02cf52a6beafffd
walid <walidrahman.officials@gmail.com>
2026-07-13 19:20:37 +0600

    solved question 01

---
+++ sqatlabpractice/welcomeMsg.java
@@ -0,0 +1,14 @@
+package sqatlabpractice;
+
+// Question 01
+
+public class welcomeMsg {
+
+    void showMessage(){
+        System.out.println("welcome to java");
+    }
+    public static void main(String[] args) {
+        welcomeMsg msg = new welcomeMsg();
+        msg.showMessage();
+    }
+}
