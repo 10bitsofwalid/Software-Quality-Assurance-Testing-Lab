3da5ba47eb8ef7e95577c29116d1ebf985bb95e5
walid <walidrahman.officials@gmail.com>
2026-07-13 20:27:16 +0600

    solved question 03

---
+++ sqatlabpractice/storeMarks.java
@@ -0,0 +1,13 @@
+package sqatlabpractice;
+
+// Question 03: Store the marks of three subjects in an integer array in the main method.
+
+public class storeMarks {
+    public static void main(String[] args) {
+        double[] marks = {2, 3, 5.5};
+
+        System.out.println("marks 1: "+marks[0]);
+        System.out.println("marks 2: "+marks[1]);
+        System.out.println("marks 3: "+marks[2]);
+    }
+}
